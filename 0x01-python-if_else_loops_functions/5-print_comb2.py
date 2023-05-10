553e65fc493b768460bb24dd6e6db9a01bf0793a
Grace-ngigi <gracengigi859@gmail.com>
2023-05-09 19:50:58 -0700

    prints numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num < 99:
-        print("{:02d}, ".format(num), end ="")
+    if num == 99:
+        print("{}".format(num))
     else:
-        print("{:02d}\n".format(num), end="")
+        print("{:02d}".format(num), end=", ")

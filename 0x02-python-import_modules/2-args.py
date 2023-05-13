bdbb737819e83717519707ffe8da01d080a4800e
Grace-ngigi <gracengigi859@gmail.com>
2023-05-13 11:17:45 -0700

    prints the result of the addition of all arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     total  = len(sys.argv) - 1
     if (total == 0):
-        print("{} arguments.".format(total))
+        print(total, "arguments.")
     elif (total == 1):
-        print("{}: argument: {}".format(total, sys.argv[0]))
+        print(total, "argument:".format(sys.argv[0]))
         print("1:", sys.argv[1])
     else:
         print("{}: arguments:".format(total))
-        for i in range (1, total + 1):
+        for i in range(1, total + 1):
             print("{}: {}".format(i, sys.argv[i]))

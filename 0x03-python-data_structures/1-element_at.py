105ffb11d812900ee74a3e35bd41de3722ab052e
Grace-ngigi <gracengigi859@gmail.com>
2023-05-13 21:32:28 -0700

    retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if (idx >= len(my_list)):
+        return (None)
+    elif (index <= 0):
+        return (None)
+    else:
+        return (my_list[idx])

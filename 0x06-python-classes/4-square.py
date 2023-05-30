f4827023810f2bedec1e1f7da16b9ce2c0430a3f
Grace-ngigi <gracengigi859@gmail.com>
2023-05-30 11:06:22 -0700

    an empty class Square that defines a square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,19 +8,19 @@ class Square:
         """Initialize the square"""
         self.__size = size
 
-        @property
-        def size(self):
-            """defin private variable size"""
-            return self.__size
+    @property
+    def size(self):
+        """defin private variable size"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """set value to size"""
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """set value to size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """find area of the sqaure"""

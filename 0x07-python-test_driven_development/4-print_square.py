2749e90d77fca3bd1a8d6f45cf46487e753628f8
Grace-ngigi <gracengigi859@gmail.com>
2023-06-10 02:04:39 -0700

    print a square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -6,7 +6,8 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     elif type(size) is float anf size < 0:
         raise TypeError("size must ne an integer")
-    for i in range(size):
-        for j in range(size):
-            print("#", end="")
-        print()
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()

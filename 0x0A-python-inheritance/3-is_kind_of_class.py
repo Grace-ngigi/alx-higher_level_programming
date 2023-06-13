875fbeb83335ffef24994f272cbf91c1acc5c902
Grace-ngigi <gracengigi859@gmail.com>
2023-06-12 20:24:29 -0700

    is object instance of the class?

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""return same class or inherit from a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Return True if it is an Instance of that class"""
+    if isInstance(obj, a_class):
+        return True
+    else:
+        return False

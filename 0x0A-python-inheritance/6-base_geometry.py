59e6d104c7afb4d78b72b72dcd9ef87f3f96fe40
Grace-ngigi <gracengigi859@gmail.com>
2023-06-13 09:10:50 -0700

    add a method that is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""An Empty base geometry class"""
+
+
+class BaseGeometry:
+    """An empty base geometry class"""
+    def area(self):
+        """public instance method for area calculations"""
+        raise Exception("area() is not implemented")

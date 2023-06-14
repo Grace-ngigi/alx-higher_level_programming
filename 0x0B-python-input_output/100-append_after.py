f9b7aaafe7bca44a91a580f76c90c22cdedb13e6
Grace-ngigi <gracengigi859@gmail.com>
2023-06-14 10:24:55 -0700

    inserts a line of text to a file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file"""
+    with open(filename, 'r') as ffile:
+        lines = f.readlines()
+    with open(filename, 'w') as ffile:
+        for line in lines:
+            if search_string in line:
+                ffile.write(line + new_string)
+            else:
+                ffile.write(line)

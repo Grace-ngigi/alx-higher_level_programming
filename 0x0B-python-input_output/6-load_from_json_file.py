391de06fdadb66416568eda1906e1fd1ab43ee4b
Grace-ngigi <gracengigi859@gmail.com>
2023-06-13 19:57:34 -0700

    load file from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creates an Object from a JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """that creates an Object from a “JSON file”"""
+    with open(filename, 'r') as ffile:
+        return json.load(ffile)

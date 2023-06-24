258b54bddd7e69f3fb8f4f940844ce2b36a23041
Grace-ngigi <gracengigi859@gmail.com>
2023-06-23 19:36:11 -0700

    display

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -68,3 +68,11 @@ class Rectangle(Base):
     def area(self):
         """find the area"""
         return self.width * self.height
+    
+    def display(self):
+        """print rectangle"""
+        for i in range(self.y):
+            print()
+        for i in range (self.height):
+            print(" " * self.x + "#" * self.width)
+

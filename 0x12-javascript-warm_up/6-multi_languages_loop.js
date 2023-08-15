976e30f1634d85dea2a715801ee3fb9a4fbfdd7b
Grace-ngigi <gracengigi859@gmail.com>
2023-08-14 19:24:09 -0700

    Loop to lnguages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// prints strings using a loop
+
+let i;
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}

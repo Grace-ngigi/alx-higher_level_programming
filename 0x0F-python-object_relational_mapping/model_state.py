1e094a1ce4d400f09b998e6a8cc05f5af4dc945d
Grace-ngigi <gracengigi859@gmail.com>
2023-08-19 18:21:03 -0700

    class definition of a State

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-import sys
+""" class definition of a State"""
+
 from enum import unique
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """define a table"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    id = Column(Integer, primary_key=True,
+                autoincrement=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-    .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)

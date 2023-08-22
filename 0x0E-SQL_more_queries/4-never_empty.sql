8a143eb36822885c68e3663186a4c68fecdcdc5b
Grace-ngigi <gracengigi859@gmail.com>
2023-08-21 19:10:26 -0700

    create table id_not_null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,6 @@
- -- create a given table
-CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256))
+-- create table id_not_null
+-- if table exists, script should fail
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);

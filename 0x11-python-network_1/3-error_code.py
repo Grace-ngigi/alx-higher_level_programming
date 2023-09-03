228872ed3fc70c6decb8e46272a264dbb577d036
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 02:45:29 -0700

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+takes in a url
+sends  request to the url
+display body of response
+manage urllib.error.HTTPError
+"""
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".fomat(e.code))
+    except urllib.error.URLError as e:
+        print("URL error: {}". format(e))

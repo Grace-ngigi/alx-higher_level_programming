76084f6ed6b8ddef35386467ba2afb1bf3a5a19d
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 03:35:54 -0700

    Error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+takes in url
+sends a request
+display body response
+if status code >= 400 print Error code
+use packges request and sys
+"""
+
+if __name__ == "__main_":
+    import sys
+    import requests
+
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print("Error code: {}".formt(req.status_code))
+    else:
+        print(req.text)

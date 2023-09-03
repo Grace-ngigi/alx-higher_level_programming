1f407913641e5d7f657b89d972a7647c92bac25f
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 02:33:35 -0700

    post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+takes URL and email
+send post request
+display the reponse decoded in utf-8
+"""
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
+
+    url = sys.argv[1]
+    email = {"email":sys.argv[2]}
+
+    data = urllib.parse.urlencode(email).encode("ascii")
+    try:
+
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(req) as res:
+            print(res.read().decode("utf-8"))
+    except urllib.error.URLError as e:
+        print("Error: {}".format(e))

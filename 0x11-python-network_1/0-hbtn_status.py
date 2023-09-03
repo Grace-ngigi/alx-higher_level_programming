7312a5c7c95a67efb7308a02622cde0c98c7ce33
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 01:51:24 -0700

    Whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as res:
             data = res.read()
             print(f"Body response:")
-            print(f"- type: {type(data)}")
-            print(f"- content: {data}")
-            print(f"{'-' * 30}")
+            print('\t- type: {}'.format(type(data)))
+            print('\t- content: {}'.format(data))
+            print('\t- utf8 content: {}'.format(data.decode("utf-8", "replace")))
     except urllib.error.URLError as e:
         print(f"Error: {e}")

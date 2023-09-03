ae6c89f621f7168fa4e9eced74ea29669ea16730
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 00:58:53 -0700

    send post request with body

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send post req with a body
-curl -sX POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" $1
+curl -s -X POST -d "email: test@gmail.com" -d "subject: I will always be here for PLD" $1

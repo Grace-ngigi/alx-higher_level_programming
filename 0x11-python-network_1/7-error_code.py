1b07d352eee5a9408d126b889d93f874ab0214a9
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 17:07:11 -0700

    search API

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -13,8 +13,13 @@ if __name__ == "__main_":
 
     url = sys.argv[1]
 
-    req = requests.get(url)
-    if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
-    else:
-        print(req.text)
+    try:
+        res = requests.get(url)
+        if res.status_code >= 400:
+            print("Error code: {}".format(res.status_code))
+        else:
+            print(res.text)
+    except requests.exceptions.RequestException as e:
+        print("Request error: {}".format(e))
+    except requests.exceptions.HTTPError as e:
+        print("Error code: {}".format(e.response.status_code))
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+takes in a leter
+sends a post request to some url
+with the letter as a prameter
+response body is Json formatted
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = "http://0.0.0.0:5000/search_user"
+
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+    else:
+        letter =""
+
+    q = {"q": letter}
+
+    try:
+        res = requests.post(url, params=q)
+        data = res.json()
+
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not  valid JSON")
+    except requests.exceptions.RequestException as e:
+        print("Request Error:", e)

4977824d678e247c8bdd18f637c024699c9f0c28
Grace-ngigi <gracengigi859@gmail.com>
2023-09-03 17:29:59 -0700

    My GitHub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+takes GitHub credentials(username and password)
+uses GitHub API to display id
+use Basic Authentiction with a personal access token as passwd
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        passwd = sys.argv[2]
+    url = "https://api.github.com/user"
+    auth = (username, passwd)
+
+    try:
+        res = requests.get(url, auth = auth)
+        if res.status_code == 200:
+            data = res.json()
+            user_id = data.get("id")
+            print(user_id)
+        else:
+            print("Request failed with status code: {}".format(res.status_code))
+    except requests.exceptions.RequestException as e:
+        print("Request Error: {}".format(e))

bc1a0d6c61300b8d3d695bc0c587f8a7ce227580
Grace-ngigi <gracengigi859@gmail.com>
2023-09-15 22:27:45 -0700

    modified insert node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -44,33 +44,5 @@ listint_t *insert_node(listint_t **head, int number)
 		prev->next = newnode;
 		newnode->next = trvs;
 	}
-	sort_list(head);
 	return (newnode);
-}
-/**
- * sort_list - sorts a given list
- * @head: pointer to the given list;s head
- * Return: pointer to the head of the sorted list
- */
-listint_t *sort_list(listint_t **head)
-{
-	listint_t *i, *j;
-	int temp;
-
-	if (*head == NULL || (*head)->next == NULL)
-		return (NULL);
-
-	for (i = *head; i != NULL; i = i->next)
-	{
-		for (j = i->next; j != NULL; j = j->next)
-		{
-			if (i->n > j->n)
-			{
-				temp = i->n;
-				i->n = j->n;
-				j->n = temp;
-			}
-		}
-	}
-	return (*head);
 }
